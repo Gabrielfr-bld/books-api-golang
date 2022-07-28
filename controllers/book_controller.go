7065158918fa3625761263e76d3a5ad4a1684cec
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-28 18:16:10 -0300

    feat: implements functions CRUD

--- controllers/book_controller.go
+++ controllers/book_controller.go
@@ -9,8 +9,124 @@ import (
 )
 
 func ShowBook(c *gin.Context) {
+	id := c.Param("id")
 
-	c.JSON(200, gin.H{
-		"value": "ok",
-	})
+	newId, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "Id has to be integer",
+		})
+
+		return 
+	}
+
+	db := database.GetDatabase()
+
+	var book models.Book
+	err = db.First(&book, newId).Error
+	if err != nil {
+		c.JSON(404, gin.H{
+			"error": "Book not found: " + err.Error(),
+		})
+
+		return
+	}
+
+	c.JSON(200, book)
+}
+
+func CreateBook(c *gin.Context) {
+	var book models.Book
+
+	err := c.ShouldBindJSON(&book)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "Invalid JSON",
+		})
+
+		return
+	}
+
+	db := database.GetDatabase()
+
+	err = db.Create(&book).Error
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "Invalid JSON",
+		})
+
+		return
+	}
+
+	c.JSON(201, book)
+}
+
+func ShowBooks(c *gin.Context) {
+	db := database.GetDatabase()
+
+	var books []models.Book
+	err := db.Find(&books).Error
+	if err != nil {
+		c.JSON(404, gin.H{
+			"error": "not found list: " + err.Error(),
+		})
+
+		return
+	}
+
+	c.JSON(200, books)
+}
+
+func UpdateBook(c *gin.Context) {
+	db := database.GetDatabase()
+
+	var book models.Book
+
+	err := c.ShouldBindJSON(&book)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "Invalid JSON",
+		})
+
+		return
+	}
+
+	err = db.Save(&book).Error
+
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "cannot update book: " + err.Error(),
+		})
+
+		return
+	}
+
+	c.JSON(200, book)
+}
+
+func DeleteBook(c *gin.C) {
+	id := c.Param("id")
+
+	newId, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "Id has to be integer",
+		})
+
+		return 
+	}
+
+	db := database.GetDatabase()
+
+	err = db.Delete(&models.Book{}, newId).Error
+
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "cannot delete book: " + err.Error(),
+		})
+
+		return
+	}
+
+	c.Status(204)
 }
