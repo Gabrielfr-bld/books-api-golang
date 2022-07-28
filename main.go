35bbfb766fd06e12f3a15afaefb80a7e0e99f9b6
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-28 18:14:09 -0300

    refactor: main.go

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"github.com/Gabrielfr-bld/books-api-golang/server"
+	"github.com/Gabrielfr-bld/books-api-golang/database"
 )
 
 func main() {
+	database.StartDB()
+
 	s := server.NewServer()
 
 	s.Run()
