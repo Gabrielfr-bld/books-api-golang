e33d82f06680a4b2c42c86fff0dcc31f699bcfdf
Gabrielfr-bld <franklingabriel158@gmail.com>
2022-07-28 18:16:32 -0300

    fix: database config

--- database/database.go
+++ database/database.go
@@ -1,13 +1,17 @@
 package database
 import (
 	"log"
+	"time"
+
+	"github.com/Gabrielfr-bld/books-api-golang/database/migrations"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var db *gorm.DB
 
 func StartDB() {
-	str := "host=localhost port=25432 user=admin dbname=books password=123456"
+	str := "host=localhost port=5432 user=admin dbname=books sslmode=disable password=123456"
 
 	database, err := gorm.Open(postgres.Open(str), &gorm.Config{})
 
@@ -21,6 +25,8 @@ func StartDB() {
 	config.SetMaxIdleConns(10)
 	config.SetMaxOpenConns(100)
 	config.SetConnMaxLifetime(time.Hour)
+
+	migrations.RunMigrations(db)
 }
 
 func GetDatabase() *gorm.DB {
